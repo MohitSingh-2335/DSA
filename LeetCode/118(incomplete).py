44f138b6e80bbd6c528c4f297c3e3d76d0e64dac
MohitSingh-2335 <2335mohitsingh@gmail.com>
2026-07-22 17:09:43 +0530

    Add incomplete draft for LeetCode 118: Pascal's Triangle

---
+++ LeetCode/118(incomplete).py
@@ -0,0 +1,34 @@
+numRows = 5
+
+a = [[1] * (i + 1) for i in range(numRows)]
+
+print(a)
+
+for j in range(numRows):
+
+    for i in a[j]:
+
+        if len(a[j]) > 2:
+
+            
+
+
+# for i in range(numRows):
+
+#     b.append(a)
+
+#     if i == 1:
+
+#         b.append(i + 1)
+
+#     elif i == 2:
+
+#         n = [1, (numRows - (numRows - 1))]
+
+#         a.append(n)
+    
+#     b.append(a)
+
+
+# print(b)
+
