2e98c6681b677cd5eed70657e0c673dcc299894c
MohitSingh-2335 <2335mohitsingh@gmail.com>
2026-07-24 18:21:26 +0530

    Completed the this using hashtable

--- Hash/3.py
+++ Hash/3.py
@@ -1,17 +1,20 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        
-        a = list(s)
-        count = []
+
+        count = 0
+        a = []
         i = 0
-        for j in range(i + 1, len(a)):
-            if a[i] == a[j]:
-                i += 1
-            else:
-                count.append(a[j])
-        return len(count)
+        hash_list = [-1] * 127
+        for j, char in enumerate(s):
+            ind = ord(char)
+            if hash_list[ind] >= i:
+                i = hash_list[ind] + 1
+            hash_list[ind] = j
 
-
+            count = j - i + 1
+            a.append(count)
+        return max(a)
+            
 s = input()
 obj = Solution()
 ans = obj.lengthOfLongestSubstring(s)
