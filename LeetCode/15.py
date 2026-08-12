2cbdd5e4ccded7d36b9ace8f0095b7462a191eae
MohitSingh-2335 <2335mohitsingh@gmail.com>
2026-08-12 16:53:45 +0530

    incomplete leetcode 15

--- LeetCode/15.py
+++ LeetCode/15.py
@@ -1,11 +1,17 @@
-nums = [-1,0,1,2,-1,-4]
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        
+        count = []
+        i = 0
+        for j in range(i + 1, len(nums)):
+            for k in range(j + 1, len(nums)):
+                if nums[i] + nums[j] + nums[k] == 0:
+                    count.append([i,j,k])
+        else:
+            i += 1
+        return count
 
-i = 0
-j = 1
-k = 2
-
-for i in range(len(nums)):
-
-    for j in range(len(nums)):
-
-        for k 
+nums = list(map(int, input().split()))
+obj = Solution()
+ans = obj.threeSum(nums)
+print(ans)
