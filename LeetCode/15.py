eb73054265a0579683b6906ae8955955e9cc9247
MohitSingh-2335 <2335mohitsingh@gmail.com>
2026-08-13 16:42:44 +0530

    completed 3sum

--- LeetCode/15.py
+++ LeetCode/15.py
@@ -1,15 +1,35 @@
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
         
+        nums.sort()
+        n = len(nums)
         count = []
-        i = 0
-        for j in range(i + 1, len(nums)):
-            for k in range(j + 1, len(nums)):
-                if nums[i] + nums[j] + nums[k] == 0:
-                    count.append([i,j,k])
-        else:
-            i += 1
-        return count
+
+        for i in range(n - 2):
+            if nums[i] > 0:
+                break
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            j = i + 1
+            k = n - 1
+
+            while (j < k):
+                total = nums[i] + nums[j] + nums[k]
+
+                if total > 0:
+                    k -= 1
+                elif total < 0:
+                    j += 1
+
+                else:
+                    count.append([nums[i],nums[j],nums[k]])
+                    j += 1
+                    k -= 1
+
+                    while nums[j] == nums[j - 1] and j < k:
+                        j += 1
+
+        return count 
 
 nums = list(map(int, input().split()))
 obj = Solution()
