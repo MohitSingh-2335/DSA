cb81be5c5c9c641b6b6e79040752b45516a8e38e
MohitSingh-2335 <2335mohitsingh@gmail.com>
2026-08-21 16:56:18 +0530

    Still incomplete

--- LeetCode/424.py
+++ LeetCode/424.py
@@ -2,18 +2,26 @@ class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
 
         start = 0
+        end = 0
         highLen = 0
         num = k
         strS = list(s)
-        for end in range(start, len(s)):
+        if len(s) < 0:
+            return 0
+        while (end < len(strS)):
             if strS[start] == strS[end] and num >= 0:
                 highLen = len(strS[start : end + 1])
+                end += 1
             elif strS[start] != strS[end] and num > 0:
                 strS[end] = strS[start]
                 highLen = len(strS[start : end + 1])
                 num -= 1
+                end += 1
             elif strS[start] != strS[end] and num == 0:
-                break
+                start += 1
+                end = start + 1
+                num = k
+                strS = list(s)
         return highLen
 
 s = input()
